c02a8382786cb6ecfe2b85995f699c69e3a6875d
mlvl42 <melvil.guillaume@gmail.com>
2022-08-04 12:57:43 +0200

    add lua lsp

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -69,12 +69,42 @@ nvim_lsp.rust_analyzer.setup {
 	}
 }
 
+-- python
 nvim_lsp.pyright.setup {
 	capabilities = capabilities,
 	on_attach = on_attach,
 	flags = {
 		debounce_text_changes = 150,
 	}
+}
+
+-- lua
+nvim_lsp.sumneko_lua.setup {
+	capabilities = capabilities,
+	on_attach = on_attach,
+	flags = {
+		debounce_text_changes = 150,
+	},
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = {'vim'},
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 }
 
 -- zk
